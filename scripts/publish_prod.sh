68d60b21af059177e9c4de9e6375ebd7a64a1735
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2024-04-04 13:32:53 -0400

    use sso for govcloud

--- scripts/publish_prod.sh
+++ scripts/publish_prod.sh
@@ -54,8 +54,8 @@ VERSION=$LAYER_VERSION ARCH=arm64 aws-vault exec sso-prod-engineering -- ./scrip
 echo
 echo "Publishing layers to GovCloud AWS regions..."
 ddsaml2aws login -a govcloud-us1-fed-human-engineering
-VERSION=$LAYER_VERSION ARCH=amd64 AWS_PROFILE=govcloud-us1-fed-human-engineering ./scripts/publish_layer.sh
-VERSION=$LAYER_VERSION ARCH=arm64 AWS_PROFILE=govcloud-us1-fed-human-engineering ./scripts/publish_layer.sh
+VERSION=$LAYER_VERSION ARCH=amd64 aws-vault exec sso-govcloud-us1-fed-engineering -- ./scripts/publish_layer.sh
+VERSION=$LAYER_VERSION ARCH=arm64 aws-vault exec sso-govcloud-us1-fed-engineering -- ./scripts/publish_layer.sh
 
 echo "Creating tag in the datadog-lambda-extension repository for release on GitHub"
 git tag "v$LAYER_VERSION"
